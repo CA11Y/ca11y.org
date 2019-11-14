d753e1547d5e3dacb5a24361836f9f74dbb8b74f
masuP9 <masuP9@gmail.com>
2019-11-14 11:53:44 +0900

    feat: add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     dir: { input: "src" },
